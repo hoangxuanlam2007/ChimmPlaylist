3f52111e029e24f06a6e425f50ba287454744ec2
Chimmyw <chim31102007@gmail.com>
2022-03-13 14:03:47 +0700

    compress images

--- assets/js/music-list.js
+++ assets/js/music-list.js
@@ -12,241 +12,241 @@ let allMusic = [
   {
     name: "0 Cần Mật",
     artist: "Fous ft. UMIE - Cukak Remix",
-    img: "music-1",
+    img: "music-1-min-min",
     src: "music-1"
   },
   {
     name: "Ánh Chiều Tàn",
     artist: "D Empty ft. Poll - 1967 Remix",
-    img: "music-2",
+    img: "music-2-min",
     src: "music-2"
   },
   {
     name: "Không Cần Cố",
     artist: "RPT MCK ft. Tlinh - Cukak Remix",
-    img: "music-3",
+    img: "music-3-min",
     src: "music-3"
   },
   {
     name: "Cớ Sao Em Buồn",
     artist: "Nguyễn Trọng Tài - Cukak Remix",
-    img: "music-4",
+    img: "music-4-min",
     src: "music-4"
   },
   {
     name: "Bật Nhạc Lên",
     artist: "HIEUTHUHAI ft. Harmonie x Cukak",
-    img: "music-5",
+    img: "music-5-min",
     src: "music-5"
   },
   {
     name: "Đau Nhất Là Lặng Im",
     artist: "Erik x Cukak Remix",
-    img: "music-6",
+    img: "music-6-min",
     src: "music-6"
   },
   {
     name: "Chạy Về Khóc Với Anh",
     artist: "Erik - Cukak Remix",
-    img: "music-7",
-    src: "music-7"
+    img: "music-7-min",
+    src: "music-7-min"
   },
   {
     name: "Ôm Em Lần Cuối",
     artist: "Nit ft. Sing - Cukak Remix",
-    img: "music-8",
+    img: "music-8-min",
     src: "music-8"
   },
   {
     name: "Em Là Hoàng Hôn",
     artist: "Vang x Cloud 5 - Frexs Remix",
-    img: "music-9",
+    img: "music-9-min",
     src: "music-9"
   },
   {
     name: "For Get Me Now",
     artist: "Fishy ft. Trí Dũng - Cukak Remix",
-    img: "music-10",
+    img: "music-10-min",
     src: "music-10"
   },
   {
     name: "Mây Đêm Chờ Mấy Đêm",
     artist: "Nguyễn Hữu Kha - Chimmyw Remix",
-    img: "music-11",
+    img: "music-11-min",
     src: "music-11"
   },
   {
     name: "Em Là Nhất",
     artist: "kis x Hoàng Kaylee x CilTee - 1967 Remix",
-    img: "music-12",
+    img: "music-12-min",
     src: "music-12"
   },
   {
     name: "Anh Đã Lạc Vào",
     artist: " GREEN ft. Truzg - Cukak Remix",
-    img: "music-13",
+    img: "music-13-min",
     src: "music-13"
   },
   {
     name: "Có Hẹn Với Thanh Xuân",
     artist: "MONSTAR x Chimmyw Remix",
-    img: "music-14",
+    img: "music-14-min",
     src: "music-14"
   },
   {
     name: "Yêu Trong Nắng Mai",
     artist: "Hồ Văn Quý x Lê Vũ - 1967 Remix",
-    img: "music-15",
+    img: "music-15-min",
     src: "music-15"
   },
   {
     name: "Tình Yêu Ngủ Quên",
     artist: "Hoàng Tôn ft. LyHan - Cukak Remix",
-    img: "music-16",
+    img: "music-16-min",
     src: "music-16"
   },
   {
     name: "Muốn Được Cùng Em",
     artist: "ft. QUỲNH GAI - Freaky x CM1X x Chimmyw",
-    img: "music-17",
+    img: "music-17-min",
     src: "music-17"
   },
   {
     name: "Muốn Nói Với Em",
     artist: "TTeam x BlackBi Liz x DT Beatz Remix",
-    img: "music-18",
+    img: "music-18-min",
     src: "music-18"
   },
   {
     name: "Có Em Là Đủ",
     artist: "Will - Cukak Remix",
-    img: "music-19",
+    img: "music-19-min",
     src: "music-19"
   },
   {
     name: "Nắng Đã Làm Má Em Thêm Hồng",
     artist: "CHARLES Cover - CM1X x Chimmyw",
-    img: "music-20",
+    img: "music-20-min",
     src: "music-20"
   },
   {
     name: "Ở Nhà Chán Quó",
     artist: "Taylor - Cukak Remix",
-    img: "music-21",
-    src: "music-21"
+    img: "music-21-min",
+    src: "music-21-min"
   },
   {
     name: "PRECIOUS",
     artist: "DADUC ft. KIPER - Cukak Remix",
-    img: "music-22",
+    img: "music-22-min",
     src: "music-22"
   },
   {
     name: "Siren",
     artist: "TGSN ft. Tlinh & RZ Mas - Cukak & Chimmyw Mix",
-    img: "music-23",
+    img: "music-23-min",
     src: "music-23"
   },
   {
     name: "3107 7",
     artist: " W/n ft. titie, Duongg - Chimmyw Remix",
-    img: "music-24",
+    img: "music-24-min",
     src: "music-24"
   },
   {
     name: "Thanh Xuân Là Anh",
     artist: " NDIÊN x 2T x CAO & Chimmyw Mix",
-    img: "music-25",
+    img: "music-25-min",
     src: "music-25"
   },
   {
     name: "Em Như Mây Gió",
     artist: "HuyR - Cukak Remix",
-    img: "music-26",
+    img: "music-26-min",
     src: "music-26"
   },
   {
     name: "Tình Đầu",
     artist: "Tăng Duy Tân - Chimmyw Mix",
-    img: "music-27",
+    img: "music-27-min",
     src: "music-27"
   },
   {
     name: "Tình Yêu Của Anh",
     artist: "Phú Quí - Cukak Remix",
-    img: "music-28",
+    img: "music-28-min",
     src: "music-28"
   },
   {
     name: "Phố Đã Lên Đèn",
     artist: "Huyền Tâm Môn - Cukak Remix",
-    img: "music-29",
+    img: "music-29-min",
     src: "music-29"
   },
   {
     name: "SUNDAY HIPHOP CYPHER",
     artist: "16 Typh, 16 BrT, Lil Wuyn, RIC - Cukak x Chimmyw Collab",
-    img: "music-30",
+    img: "music-30-min",
     src: "music-30"
   },
   {
     name: "Nhắn Tới Khoảng Trời Em",
     artist: "Quân A.P x Toann - 1967 Remix",
-    img: "music-31",
+    img: "music-31-min",
     src: "music-31"
   },
   {
     name: "Dù Cho Mai Về Sau",
     artist: "Buitruonglinh - Chimmyw x Orinn Collab",
-    img: "music-32",
+    img: "music-32-min",
     src: "music-32"
   },
   {
     name: "Nụ Cười Em Là Nắng",
     artist: "GREEN x Chimmyw Remix",
-    img: "music-33",
+    img: "music-33-min",
     src: "music-33"
   },
   {
     name: "Yêu Yếu Lòng",
     artist: "Lil Zpoet - Cukak Remix",
-    img: "music-34",
+    img: "music-34-min",
     src: "music-34"
   },
   {
     name: "Ngày Đầu Tiên",
     artist: "Đức Phúc x Chimmyw x Cukak",
-    img: "music-35",
+    img: "music-35-min",
     src: "music-35"
   },
   {
     name: "Em iu",
     artist: "Andree Right Hand ft. Wxrdie x Bình Gold x 2pillz x Cukak",
-    img: "music-36",
+    img: "music-36-min",
     src: "music-36"
   },
   {
     name: "Đôi Mắt Kẻ Si Tình",
     artist: "Young Noi$y - Cukak Remix",
-    img: "music-37",
+    img: "music-37-min",
     src: "music-37"
   },
   {
     name: "Đi Một Tí Thôi",
     artist: "Deus Tiến Đạt x Chimmyw x Cukak Mix",
-    img: "music-38",
+    img: "music-38-min",
     src: "music-38"
   },
   {
     name: "Tình Ca Tình Ta",
     artist: "kis - Cukak Remix",
-    img: "music-39",
+    img: "music-39-min",
     src: "music-39"
   },
   {
     name: "Muộn Rồi Mà Sao Còn",
     artist: "Sơn Tùng M-TP x Cukak - 1967 Remix",
-    img: "music-40",
+    img: "music-40-min",
     src: "music-40"
   },
 ];
